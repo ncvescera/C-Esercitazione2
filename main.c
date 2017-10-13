fde038043923f24086339c099571a1f4aebecbb4
Nicolo Vescera <nicolo.vescera@gmail.com>
2017-10-13 09:36:36 +0200

    Completato esercizio

--- main.c
+++ main.c
@@ -1,24 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 13 ottobre 2017, 8.59
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define TRUE 1
+#define FALSE 0
 
-    return (EXIT_SUCCESS);
+/*
+ * Funzione che stabilisce se un numero è primo
+ * 
+ * @param number Numero da stabilire se è primo
+ * @return Ritorna 1 se il numero è primo, altrimenti 0
+ */
+int isPrime(int number);
+
+int main(int argc, char** argv) {
+    int n;
+    int result;
+    
+    printf("Inserisci un numero maggiore di 0: ");
+    scanf("%d",&n);
+    
+    if(n > 0){
+        if(n == 1)
+            printf("Il numero %d e' primo",n);
+        else{
+            result = isPrime(n);
+            if(result)
+                printf("Il numero %d e' primo",n);
+            else
+                printf("Il numero %d NON e' primo",n);
+        }
+        return (EXIT_SUCCESS);
+    }
+    else {
+        printf("Il numero deve essere maggiore di 0");
+        return(EXIT_FAILURE);
+    }
 }
 
+int isPrime(int number){
+    int divisori = 0;
+    
+    for(int i = 1; i <= number; i++){
+        if(number%i == 0)
+            divisori ++;
+    }
+    
+    if(divisori > 2)
+        return FALSE;
+    else
+        return TRUE;
+}
